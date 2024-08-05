ebc763dc086f051f205c87a47626ed9e615839cd
Vedant Yelsangikar <vedantyelsangikar@vedants-air.mynetworksettings.com>
2024-08-04 21:32:06 -0400

    working commit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Room, RoomList } from './rooms';
 
+
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
@@ -11,62 +12,62 @@ export class RoomsComponent {
   hideRooms = false;
 
   rooms: Room = {
-    totalRooms : 20,
-    availableRooms :11,
-    bookedRooms : 5
+    totalRooms: 20,
+    availableRooms: 11,
+    bookedRooms: 5
   }
   numberOfRooms = this.rooms.totalRooms
 
-  roomList :RoomList [] =[
+  roomList: RoomList[] = [
     {
-      roomNumber:1,
+      roomNumber: 1,
       roomType: 'Deluxe Suite',
       amenities: 'WiFi, TV, Mini Bar, Ocean View',
       price: 250,
       photos: 'https://example.com/photos/deluxe-suite.jpg',
       checkInTime: new Date('11-Nov-2024'),
       checkOutTime: new Date('18-Dec-2024'),
-      rating : 2.62
+      rating: 2.62
     },
     {
-      roomNumber:2,
+      roomNumber: 2,
       roomType: 'Standard Room',
       amenities: 'WiFi, TV, Air Conditioning',
       price: 100,
       photos: 'https://example.com/photos/standard-room.jpg',
       checkInTime: new Date('11-Nov-2024'),
       checkOutTime: new Date('18-Dec-2024'),
-      rating : 4.8268
+      rating: 4.8268
     },
     {
-      roomNumber:3,
+      roomNumber: 3,
       roomType: 'Family Suite',
       amenities: 'WiFi, TV, Mini Bar, Kitchenette, Two Bedrooms',
       price: 300,
       photos: 'https://example.com/photos/family-suite.jpg',
       checkInTime: new Date('11-Nov-2024'),
       checkOutTime: new Date('18-Dec-2024'),
-      rating : 3.5234
+      rating: 3.5234
     },
     {
-      roomNumber:4,
+      roomNumber: 4,
       roomType: 'Single Room',
       amenities: 'WiFi, TV',
       price: 75,
       photos: 'https://example.com/photos/single-room.jpg',
       checkInTime: new Date('11-Nov-2024'),
       checkOutTime: new Date('18-Dec-2024'),
-      rating : 3.3234
+      rating: 3.3234
     },
     {
-      roomNumber:5,
+      roomNumber: 5,
       roomType: 'Presidential Suite',
       amenities: 'WiFi, TV, Mini Bar, Ocean View, Private Pool, Butler Service',
       price: 1000,
       photos: 'https://example.com/photos/presidential-suite.jpg',
       checkInTime: new Date('11-Nov-2024'),
       checkOutTime: new Date('18-Dec-2024'),
-      rating : 4.52342
+      rating: 4.52342
     }
   ]
 
