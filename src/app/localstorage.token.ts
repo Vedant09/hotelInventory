c047bc6004d4ba28dbfea78f05c2149e4f880139
Vedant Yelsangikar <vedantyelsangikar@Vedants-Air.lan>
2024-08-31 19:25:23 -0400

    local storage

--- src/app/localstorage.token.ts
+++ src/app/localstorage.token.ts
@@ -1,1 +1,9 @@
-    
+import { InjectionToken } from '@angular/core';
+
+
+export const localStorageToken = new InjectionToken<any>('local storage', {
+    providedIn : 'root',
+    factory() {
+        return localStorage;
+    },
+});
