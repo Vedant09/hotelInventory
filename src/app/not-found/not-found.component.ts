2ffe686b61ab3eff76b463e644783738fdcf1569
Vedant Yelsangikar <vedantvy98@gmail.com>
2024-09-19 21:35:03 -0400

    404 ERROR, PAGE NOT FOUND

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class NotFoundComponent {
 errorcode:string = '404!!'
-message:string = 'ERROR, PAGE NOT FOUND!!!'
+message:string = 'ERROR!!!, PAGE NOT FOUND.'
 }
